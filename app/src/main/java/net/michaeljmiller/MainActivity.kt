75776e94a2602b3b29fa91035057df218042cc37
Michael Miller <commnerd@gmail.com>
2018-03-12 23:22:05 -0600

    MainActivity list formed

--- app/src/main/java/net/michaeljmiller/MainActivity.kt
+++ app/src/main/java/net/michaeljmiller/MainActivity.kt
@@ -2,11 +2,25 @@ package net.michaeljmiller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val values = arrayOf(
+                "Food",
+                "Messages",
+                "Resume"
+        )
+
+        val listView = findViewById<ListView>(R.id.main_list_view)
+
+        var adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, values)
+
+        listView.adapter = adapter
     }
 }
